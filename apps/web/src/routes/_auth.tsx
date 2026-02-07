38ef4bf52f8d7a42d42703c8fa20c5a41cfcd4a4
yyh <yuanyouhuilyz@gmail.com>
2026-02-07 21:39:19 +0800

    Revert "tests"
    This reverts commit c1d63a14e9fee16274b49e63cfa9f71200545a8a.

--- apps/web/src/routes/_auth.tsx
+++ apps/web/src/routes/_auth.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, Outlet, redirect, useMatches } from "@tanstack/react-router";
 import * as z from "zod";
 
-export const authSearchSchema = z.object({
+const authSearchSchema = z.object({
 	redirect: z
 		.string()
 		.default("/dify")
