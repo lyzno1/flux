63607c5f9d226dc2daf4c547e7f1428066475052
yyh <yuanyouhuilyz@gmail.com>
2026-02-08 16:22:45 +0800

    Update sidebar-rail.tsx

--- apps/web/src/components/sidebar/sidebar-rail.tsx
+++ apps/web/src/components/sidebar/sidebar-rail.tsx
@@ -13,7 +13,7 @@ function SidebarRail({ className, ...props }: React.ComponentProps<"button">) {
 			tabIndex={-1}
 			onClick={toggleSidebar}
 			className={cn(
-				"absolute inset-y-0 -right-2 z-20 hidden w-4 cursor-col-resize transition-colors after:absolute after:inset-y-0 after:left-1/2 after:w-[2px] hover:after:bg-sidebar-border sm:flex",
+				"absolute inset-y-0 -right-2 z-20 hidden w-4 cursor-col-resize transition-colors after:absolute after:inset-y-0 after:left-1/2 after:w-0.5 hover:after:bg-sidebar-border sm:flex",
 				"group-data-[state=collapsed]/sidebar-wrapper:cursor-e-resize",
 				className,
 			)}
